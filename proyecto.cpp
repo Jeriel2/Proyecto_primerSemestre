abfa176cb7cd9328612f9e6d167c7a46cd7f832f
Karen Tapia <karen05506@gmail.com>
2025-08-04 23:53:57 -0500

    Correccion y funcion validar dato

--- proyecto.cpp
+++ proyecto.cpp
@@ -3,27 +3,39 @@
 using namespace std;;
 
 // Funcion para validar la opcion
-int restriccionOpcion(int min, int max) {
-    int numero;
+int restriccionOpcion(int min, int max, int numero) {
     while (true) {
-        cout << "Ingrese la opcion que desea: ";
-        cin >> numero;
-
         if (cin.fail()) { //verifica que la entrada sea un numero
 
             cin.clear(); // Limpia el error de entrada
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "Entrada no válida. Intente de nuevo.\n";
+            cout << "Opcion invalida.\n";
         } else if (numero < min || numero > max) {
             // Número fuera de rango
-            std::cout << "Número fuera del rango permitido. Intente de nuevo.\n";
+            std::cout << "Opcion invalida.\n";
         } else {
-            // Entrada válida
             return numero;
         }
     }
 }
+//validacion de datos ingresados
+int validarDato(int numero) {
+    while (true) {
+        cin >> numero;
 
+        if (cin.fail()) { // No es un número
+            cin.clear(); // limpia el error
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // descarta entrada inválida
+            cout << "No se ingreso un numero.\n";
+        }
+        else if (numero <= 0) { // No es positivo
+            cout << "El número debe ser positivo.\n";
+        }
+        else {
+            return numero; 
+        }
+    }
+}
 
 int main() {
     int opcion;
@@ -38,30 +50,34 @@ int main() {
     cout<< "4. Calculo de la socucion optima"<<endl;
     cout<< "5. Solucion grafica del problema de optimización"<<endl;
     cout<< "6. Salir del programa"<<endl;
-   // cout<<"-Opcion: "; cin>> opcion;
+    cout << "Ingrese la opcion que desea: ";
+    cin >> opcion;
+   restriccionOpcion(1,6,opcion);
     switch(opcion) {
         case 1:
-            cout << "Ingreso de precios de venta seleccionado." << endl;
-            // 
+            cout << "---Ingreso de precios de venta seleccionado.---" << endl;
+            int p1, p2;
+            cout << "Ingrese el precio de las mesas:" << endl;
+            validarDato(p1);
             break;    
         case 2:
-            cout << "Ingreso de restricciones de producción seleccionado." << endl;
+            cout << "---Ingreso de restricciones de producción seleccionado.---" << endl;
             //
             break;
         case 3:
-            cout << "Ingreso de la función de ganancia seleccionado." << endl;    
+            cout << "---Ingreso de la función de ganancia seleccionado.---" << endl;    
             //            
             break;
         case 4:
-            cout << "Cálculo de la solución óptima seleccionado." << endl;
+            cout << "---Cálculo de la solución óptima seleccionado.---" << endl;
             // 
             break;
         case 5:
-            cout << "Solución gráfica del problema de optimización seleccionado." << endl;  
+            cout << "---Solución gráfica del problema de optimización seleccionado.---" << endl;  
             //            
             break;
         case 6: 
-            cout << "Saliendo del programa." << endl;
+            cout << "---Saliendo del programa.---" << endl;
             return 0;
                 
     }
